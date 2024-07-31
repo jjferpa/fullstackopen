c19463e48750b352f6017918c64089891e2ed54b
Juanjo Fernandez <jjferpa@gmail.com>
2024-07-31 18:18:17 +0200

    Ejercicio 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,30 +1,81 @@
 import { useState } from 'react'
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
   </button>
 )
 
 
-
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positiveComments, setPositiveComments] = useState(0);
+
+  const handleGoodButton = ()=> {
+    const updatedGood = good+1;
+    setGood(updatedGood); 
+    
+    const updatedTotal = updatedGood + neutral + bad;
+    setTotal(updatedTotal);
+    
+    const updatedAverage = (updatedGood-bad)/updatedTotal;
+    setAverage(updatedAverage);
+
+    const updatedPositiveComments = (updatedGood / updatedTotal);
+    setPositiveComments(updatedPositiveComments);
+  }
+  
+  const handleNeutralButton = ()=>{
+    const updatedNeutral = neutral +1;
+    setNeutral(updatedNeutral);
+    
+    const updatedTotal = good + updatedNeutral + bad;
+    setTotal(updatedNeutral);
+    
+    const updatedAverage = (good-bad)/updatedTotal;
+    setAverage(updatedAverage);
+
+    const updatedPositiveComments = (good/ updatedTotal);
+    setPositiveComments(updatedPositiveComments);
+  }
+  
+  const handleBadButton = ()=> {
+    const updatedBad = bad +1;
+    setBad(updatedBad );
+    const updatedTotal = good + neutral + updatedBad;
+    setTotal(updatedTotal);
+    const updatedAverage = (good-updatedBad)/updatedTotal;
+    setAverage(updatedAverage);
+
+    const updatedPositiveComments = (good/ updatedTotal);
+    setPositiveComments(updatedPositiveComments);
+    
+  }
+
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text='good'  />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Button handleClick={handleGoodButton} text='good'  />
+      <Button handleClick={handleNeutralButton} text='neutral' />
+      <Button handleClick={handleBadButton} text='bad' />
 
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>
+         good {good} <br />
+         neutral {neutral}<br />
+         bad {bad}<br />
+         all {total}<br />
+         average {average}<br />
+         positive {positiveComments * 100} %
+
+
+         </p>
     </div>
   )
 }
