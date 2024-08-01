686693dca48625596491c78c0c6820ee9d8318de
Juanjo Fernandez <jjferpa@gmail.com>
2024-08-01 13:26:09 +0200

    Ejercicio 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -9,10 +9,23 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
+    'The only way to go fast, is to go well.',
   ]
-
   const [selected, setSelected] = useState(0);
+
+  const initialVotes = Array(anecdotes.length).fill(0);
+
+  const [votes, setVotes] = useState(initialVotes);
+
+  const copyVotes = [...votes];
+
+
+
+  const handleVoteAnecdote = () =>{
+    copyVotes[selected] ++;
+    setVotes(copyVotes); 
+  }
+
 
   const handleNextAnecdote = () =>{
     const randomAnecdote = Math.floor(Math.random() * anecdotes.length);
@@ -25,6 +38,7 @@ const App = () => {
   return (
     <div>
       {anecdotes[selected]}<br/>
+      <button onClick={handleVoteAnecdote}>vote</button> 
       <button onClick={handleNextAnecdote}>next anecdote</button>
     </div>
   )
