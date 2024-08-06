db591846f09a81b8bfd69597624482797f8019eb
Juanjo Fernandez <jjferpa@gmail.com>
2024-08-06 12:35:24 +0200

    Ejercicio 2.12

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -34,9 +34,14 @@ const App = () => {
       return;
     } 
 
-    setPersons(persons.concat(personObject));
-    setNewName('');
-    setNewNumber('');
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data));
+        setNewName('');
+        setNewNumber('');
+      })
+
   }
 
   const handleAddNameChange = (event) =>{
