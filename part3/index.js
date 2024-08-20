38f5e7632fcfe118f3aec880289ceff275b11985
Juanjo Fernandez <jjferpa@gmail.com>
2024-08-20 18:45:07 +0200

    Ejercicio 3.13

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 const app = express();
 
@@ -15,28 +18,7 @@ morgan.token('body', (request) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons = [];
 
 app.get('/', (request, response) => {
     response.send('<h1>Welcome to Api "persons"</h1>')
@@ -49,20 +31,20 @@ app.get('/info', (request, response) =>{
         )
 });
   
-  app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  });
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(person => {
+    response.json(person)
+  })
+})
 
   app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
+    Person.findById(request.params.id).then(person => {
+      if (person) {
+        response.json(person);
     } else {
         response.status(404).send('This item does not exists').end();
     }
-
+    });
   });
 
   app.delete('/api/persons/:id', (request, response) => {
@@ -117,7 +99,7 @@ app.post('/api/persons', (request, response) => {
   });
 
 
-  const PORT = process.env.PORT || 3001;
+  const PORT = process.env.PORT;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  });
+  })
