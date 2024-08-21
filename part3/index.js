6121c88903624ea4e1179f673c2fb1716c8cb2ce
Juanjo Fernandez <jjferpa@gmail.com>
2024-08-21 17:36:21 +0200

    Ejercicio 3.15

--- part3/index.js
+++ part3/index.js
@@ -48,11 +48,8 @@ app.get('/api/persons', (request, response) => {
   });
 
   app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
-
+    Person.findByIdAndDelete(request.params.id)
+      .then(response.status(204).end());
   });
 
   const generateNewId = () => {
