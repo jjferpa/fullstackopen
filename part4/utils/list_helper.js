2f1b6e7af4ffb57790ca6daf3660d0f2749c6288
Juanjo Fernandez <jjferpa@gmail.com>
2024-08-27 23:04:07 +0200

    Ejercicio 4.7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -16,9 +16,55 @@ const dummy = (blogs) => {
     return blogs.sort((a, b) => b.likes - a.likes)[0]
 
   }
+
+  const mostBlogs = ( blogs ) => {
+
+    const bloggers = blogs.reduce((acc, blog) => {
+      const { author } = blog;
+    
+      const authorExists = acc.find(autor => autor.author === author);
+    
+      if (authorExists) {
+        authorExists.blogs++;
+      } else {
+        acc.push({ author, blogs: 1 });
+      }
+    
+      return acc;
+    }, []);
+
+    return bloggers.sort((a, b) => b.blogs - a.blogs)[0]
+
+
+  }
+
+  const mostLikes = ( blogs ) => {
+
+    const counLikes = blogs.reduce((acc, blog) => {
+      const { author, likes } = blog;
+    
+      const authorExists = acc.find(autor => autor.author === author);
+    
+      if (authorExists) {
+        authorExists.likes += likes
+      } else {
+        acc.push({ author, likes});
+      }
+    
+      return acc;
+    }, []);
+
+    return counLikes.sort((a, b) => b.likes - a.likes)[0]
+
+
+
+  }
+
   
   module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
   }
