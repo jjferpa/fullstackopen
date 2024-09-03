958b745d40d660e9b4649aac9aa7d81e1b04df89
Juanjo Fernandez <jjferpa@gmail.com>
2024-09-03 18:05:31 +0200

    Ejercicio 4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,20 +1,39 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1})
     response.json(blogs)
 
   })
+
+  const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+      return authorization.replace('Bearer ', '')
+    }
+    return null
+  }
   
   blogsRouter.post('/', async (request, response) => {
-    const users =  await User.find({})
-    const blogUser = users[0]
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+      return response.status(400).json({ error: 'User not found' })
+  }
+    
     const blog = new Blog({
       ...request.body,
-      user: blogUser})
+      user: user._id
+    })
+
 
     if(!request.body.title){
       response.status(400).send("Bad request")
@@ -25,7 +44,6 @@ blogsRouter.get('/', async (request, response) => {
     }
 
     const result = await blog.save()
-    const user = await User.findById(blogUser._id)
     user.blogs = user.blogs.concat(result._id)
     await user.save()
     response.status(201).json(result)
