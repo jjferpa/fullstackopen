ba2f571ca4a06f6442600ff5601c4fe008fde899
Juanjo Fernandez <jjferpa@gmail.com>
2024-09-12 19:11:16 +0200

    Ejercicio 5.5 - 5.6

---
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,32 @@
+import { Notification } from "./Notification"
+
+
+export const LoginForm = ({message, handleLogin, username, password, setUsername, setPassword}) => {
+  return (
+    <>
+     <h2>Log in to application</h2>
+        <Notification message={ message }/>
+        <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  )
+}
