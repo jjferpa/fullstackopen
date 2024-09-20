23af31cc7a53da2b8ef1d47e2790877a163b54ae
Juanjo Fernandez <jjferpa@gmail.com>
2024-09-20 18:46:14 +0200

    Ejercicio 5.21

--- part5/bloglist-e2e/tests/bloglist.test.js
+++ part5/bloglist-e2e/tests/bloglist.test.js
@@ -63,14 +63,31 @@ describe('When logged in', () => {
     })
 
     test('a new blog can be liked', async ({ page }) => {
-    createBlog(page, 'Blog title 50', 'Blog author', 'https://www.google.es');
-    const createdBlog = page.locator('.blog-style').last();
+      createBlog(page, 'Blog title 50', 'Blog author', 'https://www.google.es');
+      const createdBlog = page.locator('.blog-style').last();
 
-    await createdBlog.getByRole('button', {name: 'view'}).click();
-    await createdBlog.getByRole('button', {name: 'like'}).click();
+      await createdBlog.getByRole('button', {name: 'view'}).click();
+      await createdBlog.getByRole('button', {name: 'like'}).click();
 
-    await expect(createdBlog.getByTestId('likes')).toContainText('likes 1')
-    })    
+      await expect(createdBlog.getByTestId('likes')).toContainText('likes 1');
+    })
+    
+    test('a new blog can be deleted by his user', async ({page}) => { 
+      createBlog(page, 'Blog title 50', 'Blog author', 'https://www.google.es');
+
+      const createdBlog = page.locator('.blog-style').last();
+      await createdBlog.getByRole('button', {name: 'view'}).click();
+      page.once('dialog', async (dialog) => {
+        expect(dialog.message()).toEqual('Remove blog Blog title 50 by Blog author');
+        await dialog.accept();
+      });      
+      await createdBlog.getByRole('button', { name: 'remove' }).click();
+
+      await expect(createdBlog).not.toBeVisible();
+
+
+     })
+
   })
 
 })
